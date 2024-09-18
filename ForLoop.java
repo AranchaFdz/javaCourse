35d54637da2b82733cd601247d4ad444a68cbe14
Arancha Fernandez <aferfer1978@gmail.com>
2024-09-18 20:25:14 +0200

    For loop finished

---
+++ ForLoop.java
@@ -0,0 +1,15 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        
+        // for loop = executes a block of code a limited amount of times
+
+        /* for(int i=0; i<=10; i++) { // declar three values 1.index or counter; 2.add contition; 3.increment or decrement our index 
+            System.out.println(i);
+        } */
+
+        for(int i=10; i>=0; i--) {  // if we want to decrement by onother number such as 2 (int i=10; i>=0; i-=2)
+            System.out.println(i);
+        }
+        System.out.println("Happy new year!");
+    }
+}
